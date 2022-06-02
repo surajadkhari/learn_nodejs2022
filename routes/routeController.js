6a47875a4a00e3c6b67de32d97eea734e9493161
surajadkhari <surajadkhari@gmail.com>
2022-06-02 07:09:02 +0545

    crearte route for studentController.getStudents

--- routes/routeController.js
+++ routes/routeController.js
@@ -1,5 +1,9 @@
 const express=require("express")
 const route=express.Router()
+const studentController=require("../controllers/studentController")
 
 
+route.get("/students/all",studentController.getStudents)
 
+
+module.exports=route
